e5d9c2c1cb031234c39ac29ebbab31d86cc6dcd3
psaul20 <patricksaul20@gmail.com>
2018-05-18 20:25:18 -0500

    Added FlexboxLayout and started figuring out drag and drop functionality (This will be resumed at a later time)

--- app/src/main/java/a535apps/scripturememory/MemoryExercise.java
+++ app/src/main/java/a535apps/scripturememory/MemoryExercise.java
@@ -1,27 +1,24 @@
 package a535apps.scripturememory;
 
-import android.content.Context;
-import android.support.constraint.ConstraintLayout;
-import android.support.constraint.ConstraintSet;
+import android.content.ClipData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.flexbox.FlexboxLayout;
 
 import java.util.ArrayList;
 
-import a535apps.scripturememory.MemoryPassage;
-
 public class MemoryExercise extends AppCompatActivity {
 
-    ArrayList<String> psgWords;
+    ArrayList<String> lstDropWords;
+    ArrayList<String> lstDragWords;
 
-    FlexboxLayout fbLayout;
+    FlexboxLayout fblDropZone;
+    FlexboxLayout fblDragZone;
 
     TextView txtView;
 
@@ -30,38 +27,65 @@ public class MemoryExercise extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory_exercise);
 
-        fbLayout = (FlexboxLayout) findViewById(R.id.fbLayout);
+        fblDropZone = (FlexboxLayout) findViewById(R.id.fblDropZone);
+        fblDragZone = (FlexboxLayout) findViewById(R.id.fblDragZone);
 
         ArrayList<String> testWords = new ArrayList<>(1);
         testWords.add("god");
         testWords.add("world");
         testWords.add("perish");
 
-        psgWords = new ArrayList<>();
+        lstDropWords = new ArrayList<>();
+        lstDragWords = new ArrayList<>();
 
         MemoryPassage psg = getIntent().getExtras().getParcelable(SavedPsgAdapter.PSG_KEY);
         if (psg != null)
         {
             //does this work?
             for(String word : psg.getText().trim().split(" ")){
-                psgWords.add(word);
+                lstDropWords.add(word);
             }
 
             //should this logic be moved to createpsgtextviews method?
-            for(int i = 0; i < psgWords.size(); i++ ){
+            for(int i = 0; i < lstDropWords.size(); i++ ){
                 for(String checkWord : testWords){
-                    if(psgWords.get(i).toUpperCase().equals(checkWord.toUpperCase()) ){
-                        psgWords.set(i,"+");
+                    if(lstDropWords.get(i).toUpperCase().equals(checkWord.toUpperCase()) ){
+                        lstDragWords.add(lstDropWords.get(i));
+                        lstDropWords.set(i,"+");
                     }
                 }
             }
         }
 
-        CreatePsgTextViews(psgWords);
+        CreatePsgDropViews(lstDropWords);
+
+        CreatePsgDragViews(lstDragWords);
 
     }
 
-    protected void CreatePsgTextViews (ArrayList<String> words){
+    protected void CreatePsgDropViews (ArrayList<String> words) {
+
+        for (String word : words) {
+            txtView = new TextView(this);
+
+            txtView.setTextSize(30);
+            FlexboxLayout.MarginLayoutParams lp = new FlexboxLayout.MarginLayoutParams(ViewGroup.MarginLayoutParams.WRAP_CONTENT, ViewGroup.MarginLayoutParams.WRAP_CONTENT);
+            lp.setMargins(100, 100, 100, 0);
+
+            txtView.setLayoutParams(lp);
+
+            if (!word.equals("+")) {
+                txtView.setText(word);
+            } else {
+                txtView.setText(R.string.blank_space);
+            }
+
+            fblDropZone.addView(txtView);
+
+        }
+    }
+
+    protected void CreatePsgDragViews (ArrayList<String> words){
 
         for (String word : words){
             txtView = new TextView(this);
@@ -72,14 +96,35 @@ public class MemoryExercise extends AppCompatActivity {
 
             txtView.setLayoutParams(lp);
 
-            if(!word.equals("+")){
-                txtView.setText(word);
-            }
-            else {
-                txtView.setText(R.string.blank_space);
-            }
+            txtView.setText(word);
+            txtView.setTag(word);
+            txtView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event){
 
-            fbLayout.addView(txtView);
+                    if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                        ClipData cdDrag = ClipData.newPlainText(v.getTag().toString(), v.getTag().toString());
+
+                        View.DragShadowBuilder dragShadow = new MemExDragShadowBuilder(txtView);
+
+                        v.startDrag(cdDrag,  // the data to be dragged
+                                dragShadow,  // the drag shadow builder
+                                null,      // no need to use local data
+                                0          // flags (not currently used, set to 0)
+                        );
+                        return true;
+                    }
+
+                    else
+                        return false;
+
+
+
+
+                }
+            });
+
+            fblDragZone.addView(txtView);
 
         }
     }
