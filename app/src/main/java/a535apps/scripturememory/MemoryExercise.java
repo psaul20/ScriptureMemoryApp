8c732d4785a950f9b633fd6e8258fcb800940c03
psaul20 <patricksaul20@gmail.com>
2018-05-18 20:54:43 -0500

    Added comments to unfinished MemoryExerciseActivity

--- app/src/main/java/a535apps/scripturememory/MemoryExercise.java
+++ app/src/main/java/a535apps/scripturememory/MemoryExercise.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+//For documentation go to https://github.com/google/flexbox-layout
 import com.google.android.flexbox.FlexboxLayout;
 
 import java.util.ArrayList;
@@ -27,28 +28,33 @@ public class MemoryExercise extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory_exercise);
 
+        //Connect xml views to java code
         fblDropZone = (FlexboxLayout) findViewById(R.id.fblDropZone);
         fblDragZone = (FlexboxLayout) findViewById(R.id.fblDragZone);
 
+        //Determine which words will be removed to be dragged and dropped onto
         ArrayList<String> testWords = new ArrayList<>(1);
         testWords.add("god");
         testWords.add("world");
         testWords.add("perish");
 
+        //Instantiate word holder arrays
         lstDropWords = new ArrayList<>();
         lstDragWords = new ArrayList<>();
 
+        //Retrieve selected passage from intent parcelable
         MemoryPassage psg = getIntent().getExtras().getParcelable(SavedPsgAdapter.PSG_KEY);
         if (psg != null)
         {
-            //does this work?
+            //Convert all words in the psg string to a list
             for(String word : psg.getText().trim().split(" ")){
                 lstDropWords.add(word);
             }
 
-            //should this logic be moved to createpsgtextviews method?
+            //Check each word in testwords list against words in the retrieved psg
             for(int i = 0; i < lstDropWords.size(); i++ ){
                 for(String checkWord : testWords){
+                    //If there is a match, replace the word with drag and drop word indicator
                     if(lstDropWords.get(i).toUpperCase().equals(checkWord.toUpperCase()) ){
                         lstDragWords.add(lstDropWords.get(i));
                         lstDropWords.set(i,"+");
@@ -65,21 +71,30 @@ public class MemoryExercise extends AppCompatActivity {
 
     protected void CreatePsgDropViews (ArrayList<String> words) {
 
+        //For each word in psg, create a TextView and add it to the flexboxlayout
         for (String word : words) {
+
+            //Create TextView
             txtView = new TextView(this);
 
+            //Customize TextView properties
             txtView.setTextSize(30);
+
+            //Create and customize Flexbox child view Params
             FlexboxLayout.MarginLayoutParams lp = new FlexboxLayout.MarginLayoutParams(ViewGroup.MarginLayoutParams.WRAP_CONTENT, ViewGroup.MarginLayoutParams.WRAP_CONTENT);
             lp.setMargins(100, 100, 100, 0);
 
+            //Set LayoutParams to TxtView
             txtView.setLayoutParams(lp);
 
+            //If the word is a special word, replace with blank space to indicate droppable area
             if (!word.equals("+")) {
                 txtView.setText(word);
             } else {
                 txtView.setText(R.string.blank_space);
             }
 
+            //Add txtView to FlexBox
             fblDropZone.addView(txtView);
 
         }
