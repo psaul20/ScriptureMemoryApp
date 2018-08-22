ad15d73701cd5ce14558838c5aa883f335b4ba1a
psaul20 <patricksaul20@gmail.com>
2018-08-22 12:29:54 -0500

    Added show me button and cardview expansion transition to memory exercise

--- app/src/main/java/com/scripturememory/activities/MemoryExercise.java
+++ app/src/main/java/com/scripturememory/activities/MemoryExercise.java
@@ -1,9 +1,11 @@
 package com.scripturememory.activities;
 
-import android.icu.lang.UProperty;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.transition.TransitionManager;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,12 +15,25 @@ import com.scripturememory.models.MemoryPassage;
 import com.scripturememory.R;
 import com.scripturememory.adapters.SavedPsgAdapter;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MemoryExercise extends AppCompatActivity {
 
     boolean blnSuccess;
     boolean blnUpdateDb;
     MemoryPassage psg;
     long lngExercStartMillis;
+    List<String> lstWords;
+    TextView txtPsgRef;
+    TextView txtPsgTextPreview;
+    TextView txtPsgTextFull;
+    Button btnSuccess;
+    Button btnFail;
+    Button btnShowMe;
+    ViewGroup rootLayout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +42,43 @@ public class MemoryExercise extends AppCompatActivity {
 
        lngExercStartMillis = System.currentTimeMillis();
 
-        //Retrieve appropriate passage from intent parcelable
+        //Retrieve parceled passage from intent parcelable
         psg = getIntent().getExtras().getParcelable(SavedPsgAdapter.PSG_KEY);
 
         if (psg != null) {
-            TextView txtPsgRef = findViewById(R.id.txtPsgRef);
-            TextView txtPsgText = findViewById(R.id.txtPsgText);
-            Button btnSuccess = findViewById(R.id.btnSuccess);
-            Button btnFail = findViewById(R.id.btnFail);
+            txtPsgRef = findViewById(R.id.txtPsgRef);
+            txtPsgTextPreview = findViewById(R.id.txtPsgTextPreview);
+            txtPsgTextFull = findViewById(R.id.txtPsgTextFull);
+            btnSuccess = findViewById(R.id.btnSuccess);
+            btnFail = findViewById(R.id.btnFail);
+            btnShowMe = findViewById(R.id.btnShowMe);
 
+            rootLayout = findViewById(R.id.root_layout);
+
+            lstWords= Arrays.asList(psg.getText().split(" "));
 
             txtPsgRef.setText(psg.getPsgReference());
+            txtPsgTextPreview.setText(getPsgTxtPreview(lstWords));
 
-            txtPsgText.setText(psg.getText());
+            btnShowMe.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view){
+
+                    //Records all layout changes and applies auto transitions to them on
+                    //redraw
+                    TransitionManager.beginDelayedTransition(rootLayout);
+
+                    btnShowMe.setVisibility(View.GONE);
+                    btnSuccess.setVisibility(View.VISIBLE);
+                    btnFail.setVisibility(View.VISIBLE);
+
+                    txtPsgTextFull.setText(getFullPsgTxt(lstWords));
+                    txtPsgTextPreview.setVisibility(View.INVISIBLE);
+                    txtPsgTextFull.setVisibility(View.VISIBLE);
+
+                }
+
+            });
 
             btnSuccess.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -76,6 +115,34 @@ public class MemoryExercise extends AppCompatActivity {
 
         finish();
     }
+
+    private String getPsgTxtPreview(List<String> lstWords){
+
+        StringBuilder stringBuilder = new StringBuilder("");
+
+        for (int i = 0; i < 2; i++) {
+            stringBuilder.append(lstWords.get(i));
+            stringBuilder.append(" ");
+        }
+        stringBuilder.append(lstWords.get(2));
+        stringBuilder.append("...");
+
+        return stringBuilder.toString();
+
+    }
+
+    private String getFullPsgTxt (List<String> lstWords) {
+        StringBuilder stringBuilder = new StringBuilder("");
+
+        for (String word: lstWords) {
+            stringBuilder.append(word);
+            stringBuilder.append(" ");
+        }
+
+        return stringBuilder.toString().trim();
+    }
+
+
 }
 
 
